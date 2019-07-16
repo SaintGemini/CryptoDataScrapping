838e5da9a6f0df3e4fb3dcf91692f7671ecee4a8
SaintGemini <chacone96@gmail.com>
2019-07-16 14:41:48 -0700

    Update CryptoDataScrapping.py

--- CryptoDataScrapping.py
+++ CryptoDataScrapping.py
@@ -14,14 +14,21 @@ page_soup = soup(page_html, "html.parser")
 #grabs each product
 containers = page_soup.findAll("tr",{"class":"simpTblRow Bgc($extraLightBlue):h BdB Bdbc($finLightGrayAlt) Bdbc($tableBorderBlue):h H(32px) Bgc(white) "})
 
-#gets every company name and price of crypto on yahoo finance
-for container in containers:
-    
-    #gets company name
-    company = container.td.a["title"]
-    
-    #gets current price
-    price_container = container.findAll("span",{"class": "Trsdu(0.3s) "})
-    price = price_container[0].text
-    
-    print(company + "  " + "$"+ price)
+
+def get_data():
+    for container in containers:
+        #gets company name
+        company = container.td.a["title"]
+        #gets current price
+        price_container = container.findAll("span",{"class": "Trsdu(0.3s) "})
+        price = price_container[0].text
+        #gets volume in currency
+        volume_container = container.findAll("td", {"class": "Va(m) Ta(end) Pstart(20px) Pend(10px) W(120px) Fz(s)"})
+        volume = volume_container[0].text
+        #gets percent change
+        percent_container = container.findAll("td", {"class" : "Va(m) Ta(end) Pstart(20px) Fw(600) Fz(s)"})
+        percent = percent_container[2].text
+        
+        print(company + "    $" + price + "     " + percent + "     Vol: " + volume)
+        
+get_data()
